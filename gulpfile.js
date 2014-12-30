328fa23de8e9db9e55cd3bc9f34b7d3699ed9cf4
Stefan Baumgartner <sbaumg@gmail.com>
2014-12-30 11:51:20 +0100

    new, sourcemap free build

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,16 @@ var gulp = require('gulp'),
     prefix = require('gulp-autoprefixer'),
     cssmin = require('gulp-cssmin');
 
+gulp.task('griddler-dev', function() {
+  return gulp.src('src/griddler.scss')
+    .pipe(sass({ style: 'compact', sourcemap: true }))
+    .pipe(prefix('last 1 version', '> 1%', 'ie 8', 'ie 7'))
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('griddler-min', function() {
   return gulp.src('src/griddler.scss')
-    .pipe(sass({ style: 'compact', sourcemap: true }))
+    .pipe(sass({ style: 'compact', sourcemap: false }))
     .pipe(prefix('last 1 version', '> 1%', 'ie 8', 'ie 7'))
     .pipe(cssmin())
     .pipe(gulp.dest('dist'));
